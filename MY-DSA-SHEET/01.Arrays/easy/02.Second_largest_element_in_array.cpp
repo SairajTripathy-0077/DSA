ac751974a1f41841b952767559c5a0804e629ad2
Sairaj Tripathy <sairajtripathy4@gmail.com>
2026-05-28 21:26:14 +0530

    second question

--- MY-DSA-SHEET/01.Arrays/easy/02.Second_largest_element_in_array.cpp
+++ MY-DSA-SHEET/01.Arrays/easy/02.Second_largest_element_in_array.cpp
@@ -12,3 +12,22 @@ Explanation: The largest element of the
 array is 35 and the second largest element
 is 34.
 */
+#include <iostream>
+using namespace std;
+
+int SecondLargest(int arr[], int n){
+    int largest = 1, second_largest = -1;
+    for (int i = 1; i < n; i++)
+    {
+        if(arr[i] > largest){
+            second_largest = largest;
+            largest = arr[i];
+        }
+    }
+    return second_largest;
+}
+int main(){
+    int arr[] = {1,2,3,4,5,6,5,5,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << SecondLargest(arr, n);
+}
