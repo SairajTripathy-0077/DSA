19048cf37c8c52d94c5f04fcaeefd26ee5799eda
Sairaj Tripathy <sairajtripathy4@gmail.com>
2026-09-06 11:29:59 +0530

    Time: 6 ms (87.31%), Space: 45.1 MB (78.61%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,20 @@
-class Solution {
-    public int evalRPN(String[] tokens) {
+class Solution{
+        public int evalRPN(String[] tokens) {
         Stack<Integer> stk = new Stack<>();
-        for(String ch : tokens){
-            if(ch.equals("+")){
-                int c1 = (int)stk.pop();
-                int c2 = (int)stk.pop();
-                stk.push(c1 + c2);
-            }
-            else if(ch.equals("-")){
-                int c1 = (int)stk.pop();
-                int c2 = (int)stk.pop();
-                stk.push(c2 - c1);
-            }
-            else if(ch.equals("*")){
-                int c1 = (int)stk.pop();
-                int c2 = (int)stk.pop();
-                stk.push(c1 * c2);
-            }
-            else if(ch.equals("/")){
-                int c1 = (int)stk.pop();
-                int c2 = (int)stk.pop();
-                stk.push(c2 / c1);
-            }else{
-                stk.push(Integer.parseInt(ch));
+
+        for (String token : tokens) {
+            switch (token) {
+                case "+", "-", "*", "/" -> {
+                    int c1 = stk.pop();
+                    int c2 = stk.pop();
+                    stk.push(switch (token) {
+                        case "+" -> c2 + c1;
+                        case "-" -> c2 - c1;
+                        case "*" -> c2 * c1;
+                        default  -> c2 / c1; 
+                    });
+                }
+                default -> stk.push(Integer.parseInt(token));
             }
         }
         return stk.pop();
